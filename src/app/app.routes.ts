872804f4a01987d8e8f667b13c137f4ad6959739
Vinod Prajapati <78250670+vinodprajapati143@users.noreply.github.com>
2025-08-14 22:06:24 +0530

    changes done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', redirectTo: '/home', pathMatch: 'full' },
+    // { path: 'home', component: HomeComponent },
+    // { path: 'about', component: AboutComponent },
+    // { path: '**', redirectTo: '/home' }
+];
