fdeed16eada3d02a5fa9a3868060aa50beffc7b1
Dheerendra-Shar <94459386+Dheerendra-Shar@users.noreply.github.com>
2025-08-20 22:38:33 +0530

    changes

--- AK-BOSS_FE/src/environments/environment.prod.ts
+++ AK-BOSS_FE/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://api.example.com'
-  apiUrl: 'https://ak247pro.online/backend'
+  apiUrl: 'https://ak247pro.com/backend'
 
 };
