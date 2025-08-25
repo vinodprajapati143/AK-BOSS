ba38c91f124c0e946c5bd10d7e0caaace2168fba
Dheerendra-Shar <94459386+Dheerendra-Shar@users.noreply.github.com>
2025-08-25 10:44:38 +0530

    extract bearer from token

--- AK-B0SS_BE/middlewares/authMiddleware.js
+++ AK-B0SS_BE/middlewares/authMiddleware.js
@@ -8,7 +8,10 @@ exports.verifyToken = (req, res, next) => {
 
   if (!token)
     return res.status(401).json({ message: 'Access denied. Token missing.' });
-
+  // Agar token "Bearer <token>" form me hai to sirf token part nikalo
+  if (token.startsWith("Bearer ")) {
+    token = token.slice(7, token.length); // "Bearer " ke baad ka part
+  }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // req.user me data aa jayega
