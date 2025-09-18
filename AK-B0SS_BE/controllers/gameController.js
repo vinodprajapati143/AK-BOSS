f59b850319a0ac778d0469fb90c74415fa03677a
Dheerendra-Shar <94459386+Dheerendra-Shar@users.noreply.github.com>
2025-09-18 16:03:02 +0530

    changes

--- AK-B0SS_BE/controllers/gameController.js
+++ AK-B0SS_BE/controllers/gameController.js
@@ -1365,34 +1365,38 @@ exports.getUserBoardGames = async (req, res) => {
         !missingCloseInput)
       ) {
         status = "close";
-        result = ["XXX", "XX", "XXX"].join("-");
+      result = [
+      input.patte1 || "XXX",
+      (input.patte1_open || "X") + (input.patte2_close || "X"),
+      input.patte2 || "XXX"]
       }
       
-      else if (
-        isNewDay &&
-        (insideOpenWindow || insideCloseWindow || insideOpenGracePeriod || insideCloseGracePeriod)
-      ) {
-        status = "close";
-        result = ["XXX", "XX", "XXX"].join("-");
-      } else if (openWindowStarted && missingOpenInput) {
-        status = "close";
-        result = ["XXX", "X" + (input.patte2_close || "X"), input.patte2 || "XXX"].join("-");
-      } else if (closeWindowStarted && missingCloseInput) {
-        status = "play";
-        result = [input.patte1 || "XXX", (input.patte1_open || "X") + "X", "XXX"].join("-");
-      } else if (missingOpenInput && nowIST > openDateTime) {
-        status = "close";
-        result = ["XXX", "X" + (input.patte2_close || "X"), input.patte2 || "XXX"].join("-");
+      // else if (
+      //   isNewDay &&
+      //   (insideOpenWindow || insideCloseWindow || insideOpenGracePeriod || insideCloseGracePeriod)
+      // ) {
+      //   status = "close";
+      //   result = ["XXX", "XX", "XXX"].join("-");
+      // } else if (openWindowStarted && missingOpenInput) {
+      //   status = "close";
+      //   result = ["XXX", "X" + (input.patte2_close || "X"), input.patte2 || "XXX"].join("-");
+      // } else if (closeWindowStarted && missingCloseInput) {
+      //   status = "play";
+      //   result = [input.patte1 || "XXX", (input.patte1_open || "X") + "X", "XXX"].join("-");
+      // } else if (missingOpenInput && nowIST > openDateTime) {
+      //   status = "close";
+      //   result = ["XXX", "X" + (input.patte2_close || "X"), input.patte2 || "XXX"].join("-");
 
-      } else if (missingCloseInput && nowIST > closeDateTime) {
-        status = "close";
-        result = [input.patte1 || "XXX", (input.patte1_open || "X") + "X", "XXX"].join("-");
+      // } else if (missingCloseInput && nowIST > closeDateTime) {
+      //   status = "close";
+      //   result = [input.patte1 || "XXX", (input.patte1_open || "X") + "X", "XXX"].join("-");
 
-      } else if (formattedInputDate === yesterdayDate && !missingOpenInput && missingCloseInput) {
-        status = "close";
-        result = [input.patte1 || "XXX", (input.patte1_open || "X") + "X", "XXX"].join("-");
+      // } else if (formattedInputDate === yesterdayDate && !missingOpenInput && missingCloseInput) {
+      //   status = "close";
+      //   result = [input.patte1 || "XXX", (input.patte1_open || "X") + "X", "XXX"].join("-");
 
-      } else {
+      // }
+       else {
         status = "Play";
       }
 
