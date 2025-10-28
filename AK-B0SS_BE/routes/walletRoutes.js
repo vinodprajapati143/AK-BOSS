97d42b0fddfef8f572044fe99ebc42de44e63d3f
Dheerendra-Shar <94459386+Dheerendra-Shar@users.noreply.github.com>
2025-10-28 15:24:23 +0530

    changes

--- AK-B0SS_BE/routes/walletRoutes.js
+++ AK-B0SS_BE/routes/walletRoutes.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const walletController = require("../controllers/walletController");
 const { verifyToken } = require("../middlewares/authMiddleware");
-
+const urlencodedParser = express.urlencoded({ extended: true });
 
 router.get('/balance', verifyToken, walletController.getUserWalletBalance);// 10/08/2025 -----> Paid
 router.get('/check-order-status', verifyToken, walletController.checkOrderStatus);
 
 router.post('/create-order', verifyToken, walletController.createAddMoneyOrder);
-router.post('/webhook', walletController.ekqrWebhook);
+router.post('/webhook', urlencodedParser, walletController.ekqrWebhook);
 
 
 
