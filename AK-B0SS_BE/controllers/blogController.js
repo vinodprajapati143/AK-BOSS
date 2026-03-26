3a97774fbfb631fec6e6d56e738c339e12ef4bae
Dheerendra-Shar <94459386+Dheerendra-Shar@users.noreply.github.com>
2026-03-27 00:17:56 +0530

    create blog changes

--- AK-B0SS_BE/controllers/blogController.js
+++ AK-B0SS_BE/controllers/blogController.js
@@ -6,17 +6,23 @@ dotenv.config();
 exports.createBlog = async (req, res) => {
   try {
     const { title, description } = req.body;
+    const userId = req.user?.id; 
 
     const image = req.file
     ? `${process.env.BASE_URL}/backend/api/uploads/${req.file.filename}`
     : '';
 
     const query = `
-      INSERT INTO blogs (title, description, image)
-      VALUES (?, ?, ?)
+      INSERT INTO blogs (user_id, title, description, image)
+      VALUES (?, ?, ?, ?)
     `;
 
-    const [result] = await db.execute(query, [title, description, image]);
+      const [result] = await db.execute(query, [
+      userId,
+      title,
+      description,
+      image
+    ]);
 
     res.status(201).json({
       message: 'Blog created successfully',
